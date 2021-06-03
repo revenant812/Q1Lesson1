0e1f04200ee2b111e887fdc977c50d6c7bc0610c
Eskin Dmitriy <9255151@mail.ru>
2021-06-03 13:27:36 +0300

    готовое решение ДЗ

--- Q1Lesson1/Program.cs
+++ Q1Lesson1/Program.cs
@@ -6,7 +6,18 @@ namespace Q1Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите Ваше имя");           
+            Console.WriteLine("Введите Ваше имя");
+            string name = Console.ReadLine();
+            /*
+            int day = DateTime.Now.Day;
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+            Console.WriteLine($"Привет, %{name}%, сегодня %{DateTime.Now}%");
+            */
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}"); //дата с временем
+            Console.WriteLine("Привет, " + name + ", " + "сегодня " + DateTime.Now.ToShortDateString()); //только дата без времени
+            Console.WriteLine($"Привет, %{name}%, сегодня %{DateTime.Now.ToShortDateString()}%"); //строка с %
+            //Console.WriteLine($"Привет {name}, сегодня 0{day}.0{month}.{year}");
         }
     }
 }
